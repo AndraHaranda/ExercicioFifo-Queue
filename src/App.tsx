4822c328fcaf6e4a2041be1561208fdd8c798d10
Andraerlem Haranda <andraerleemharandaa@gmail.com>
2020-08-27 00:37:37 -0300

    commit do exercicio de fifo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css'
 
 function App() {
+  const[fila, SetFila] = useState<string[]>([])
+  const[texto, SetTexto] = useState<string>('')
+  function enqueue(item:string){
+    SetFila([...fila,item])
+  }
+  function dequeue(){
+    SetFila(fila.slice(1))
+
+  }
+  function peek():string{
+    return fila[0]
+  }
+  function handleEnqueue(){
+    enqueue(texto)
+
+  }
+  function handleDequeue(){
+    dequeue()
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input onChange={(e)=>{SetTexto(e.target.value)}} type="text"/>
+      <button onClick={handleEnqueue}>Adicionar</button>
+      <button onClick={handleDequeue}>Remover</button>
+  <p>Proxima:{peek()}</p>
+  <ul>
+    {
+      fila.map((item)=>{
+      return <li key={item}>{item}</li>
+      })
+    }
+  </ul>
     </div>
   );
 }
